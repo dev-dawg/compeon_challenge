fde5c1ce5d13002bb8b0c654e066f2ce0fb2f785
jherrmann <jherrmann@fidor.com>
2020-10-31 17:17:27 +0100

    Add spec with context more than 160 characters

--- spec/lib/coding_challenge_spec.rb
+++ spec/lib/coding_challenge_spec.rb
@@ -7,9 +7,18 @@ RSpec.describe'Coding Challenge', :aggregate_failures do
     end
 
     it 'calls the SMS Carrier' do
-      send_sms_message('hello', 'you', 'to')
+      send_sms_message('hello from Compeon', 'hello@you.de', 'compeon@compeon.de')
 
       expect(SMS_CARRIER).to have_received(:deliver_message)
+    end
+
+    context 'when the text has more than 160 characters' do
+      it 'splits the text and sends it separately' do
+        text = 'a' * 165
+        send_sms_message(text, 'hello@you.de', 'compeon@compeon.de')
+
+        expect(SMS_CARRIER).to have_received(:deliver_message).twice
+      end
     end
   end
 end
