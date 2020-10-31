084843698a602592f03de6fa2f31f5e22212a284
jherrmann <jherrmann@fidor.com>
2020-10-31 17:35:15 +0100

    Modify spec to check for message suffix

--- spec/lib/coding_challenge_spec.rb
+++ spec/lib/coding_challenge_spec.rb
@@ -19,6 +19,20 @@ RSpec.describe'Coding Challenge', :aggregate_failures do
 
         expect(SMS_CARRIER).to have_received(:deliver_message).twice
       end
+
+      it 'adds - Part X of Y to each message' do
+        text = 'a' * 165
+        to = 'hello@you.de'
+        from = 'compeon@compeon.de'
+
+        expected_message_1 = "#{'a' * 160}#{' - Part 1 of 2'}"
+        expected_message_2 = "#{'a' * 5}#{' - Part 2 of 2'}"
+
+        send_sms_message(text, to, from)
+
+        expect(SMS_CARRIER).to have_received(:deliver_message).with(expected_message_1, to, from).once
+        expect(SMS_CARRIER).to have_received(:deliver_message).with(expected_message_2, to, from).once
+      end
     end
   end
 end
