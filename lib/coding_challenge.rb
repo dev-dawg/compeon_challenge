02ddd2879b1ef2bdf066fc3b910ee3e26b272cec
jherrmann <jherrmann@fidor.com>
2020-10-31 17:18:17 +0100

    Add method that splits messages over 160 characters

--- lib/coding_challenge.rb
+++ lib/coding_challenge.rb
@@ -6,7 +6,10 @@
 
 # You need to fix this method, currently it will crash with > 160 char messages.
 def send_sms_message(text, to, from)
-  deliver_message_via_carrier(text, to, from)
+  messages = text.scan(/.{1,160}/)
+  messages.each do |message|
+    deliver_message_via_carrier(message, to, from)
+  end
 end
 
 # This method actually sends the message via a SMS carrier
